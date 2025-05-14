639dd8c425fe9c3d1a4e13cd8fd0969982e0166d
Lynn <koliilynn@gmail.com>
2025-05-14 09:34:00 +0300

    Implement Fibonacci sequence generator in print_fibonacci function

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
+
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = []
+    if length >= 1:
+        fibonacci_sequence.append(0)
+    if length >= 2:
+        fibonacci_sequence.append(1)
+    
+    for i in range(2, length):
+        next_number = fibonacci_sequence[i-1] + fibonacci_sequence[i-2]
+        fibonacci_sequence.append(next_number)
+    
+    print(fibonacci_sequence)
